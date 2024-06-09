06a9f7a7058f48ad237ae13424d0534e4d0017ea
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2024-06-09 12:31:39 +0300

    badges for cook time and seravings

--- src/components/recipe/RecipeCard.tsx
+++ src/components/recipe/RecipeCard.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { SafeRecipe, SafeUser } from "~/app/types";
 import HeartButton from "../heart-button";
+import { Badge } from "../ui/badge";
+import { FaRegClock, FaUtensils } from "react-icons/fa";
 
 interface RecipeCardProps {
   data: SafeRecipe;
@@ -17,7 +19,7 @@ const RecipeCard = ({ data, currentUser }: RecipeCardProps) => {
       <div
         onClick={() => router.push(`/listings/${data.id}`)}
         key={data.id}
-        className="group col-span-1 cursor-pointer"
+        className="group col-span-1 cursor-pointer rounded-lg shadow-lg"
       >
         <div className="flex w-full flex-col gap-2">
           <div className="relative aspect-square w-full overflow-hidden rounded-xl">
@@ -30,12 +32,29 @@ const RecipeCard = ({ data, currentUser }: RecipeCardProps) => {
             <div className="absolute right-3 top-3">
               <HeartButton recipeId={data.id} currentUser={currentUser} />
             </div>
+            <div className="absolute bottom-1 left-2 flex gap-1">
+              <Badge variant="default">
+                <div className="flex items-center gap-1">
+                  <FaRegClock size={14} />
+                  {data.cookTime} m.
+                </div>
+              </Badge>
+              <Badge variant="default">
+                <div className="flex items-center gap-1">
+                  <FaUtensils size={14} />
+                  {data.servings}
+                </div>
+              </Badge>
+            </div>
+          </div>
+          <div className="px-2 pb-2">
+          <div className="font-light text-neutral-500">
+            {Array.isArray(data.category)
+              ? data.category.join(", ")
+              : data.category}
           </div>
           <div className="text-lg font-semibold">{data.title}</div>
-          <div className="font-light text-neutral-500">{data.category}</div>
-          <div className="flex flex-row items-center gap-1">
-            <div className="font-semibold">${data.calories}</div>
-            {data.cookTime}
+     
           </div>
           {/* {onAction && actionLabel && (
               <Button
