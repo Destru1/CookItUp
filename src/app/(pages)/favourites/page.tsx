5c77e3719f0c84a56bc4fd5bcc31f9d413844e39
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2024-06-15 21:26:51 +0300

    empty states on other pages

--- src/app/(pages)/favourites/page.tsx
+++ src/app/(pages)/favourites/page.tsx
@@ -2,10 +2,23 @@ import ClientOnly from "~/components/client-only";
 import FavouritesClient from "./favourites-client";
 import getCurrentUser from "~/app/actions/getCurrentUser";
 import getFavouriteRecipes from "~/app/actions/getFavouriteRecipes";
+import EmptyState from "~/components/empty-state";
 
 const FavourtiesPage = async () => {
   const currentUser = await getCurrentUser();
   const recipes = await getFavouriteRecipes();
+
+  if (recipes.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favourites found."
+          description="Looks like you have no favourite recipes."
+        />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
       <FavouritesClient recipes={recipes} currentUser={currentUser} />
--- src/app/(pages)/my-recipes/page.tsx
+++ src/app/(pages)/my-recipes/page.tsx
@@ -2,10 +2,20 @@ import ClientOnly from "~/components/client-only";
 import MyRecipesClient from "./my-recipes-client";
 import getCurrentUser from "~/app/actions/getCurrentUser";
 import getRecipes from "~/app/actions/getRecipes";
+import EmptyState from "~/components/empty-state";
 
 const MyRecipesPage = async () => {
   const currentUser = await getCurrentUser();
   const recipes = await getRecipes({ userId: currentUser?.id });
+
+  if (recipes.length === 0) {
+    <ClientOnly>
+      <EmptyState
+        title="No recipes found."
+        description="Your recipes will appear here."
+      />
+    </ClientOnly>;
+  }
   return (
     <ClientOnly>
       <MyRecipesClient recipes={recipes} currentUser={currentUser} />
